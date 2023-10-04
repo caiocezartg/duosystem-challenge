5dde284af2f46f72a2d66b3bd4a68883f1a89bbc
Caio Cezar <caiocezartg@gmail.com>
2023-10-04 02:07:19 -0300

    style: add global theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./styles/theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme} resetCSS>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
